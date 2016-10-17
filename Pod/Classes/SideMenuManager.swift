ea3f998d4147be1eda2bedd5faf793aab9fedd60
Andrey Kubancev <akubancev@smedialink.com>
2016-10-17 11:37:16 +0300

    merged version 1.1.10

--- Pod/Classes/SideMenuManager.swift
+++ Pod/Classes/SideMenuManager.swift
@@ -87,7 +87,7 @@ public class SideMenuManager {
     /// Draws the `menuAnimationBackgroundColor` behind the status bar. Default is true.
     public static var menuFadeStatusBar = true
 
-    /// Makes presenting ViewController pop once from the NavigationController stack after presenting SideMenu. Default to false.
+    /// Makes presenting ViewController pop once from the NavigationController stack after presenting SideMenu. Also shows the navigation bar. Default to false.
     public static var forcePresentingViewControllerToPop = false
     
     /// - Warning: Deprecated. Use `menuAnimationTransformScaleFactor` instead.
